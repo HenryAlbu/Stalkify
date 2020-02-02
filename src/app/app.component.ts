8966179fcf769d6faad482b09070c9270c3846c7
henryalbu <albuquerque_henry@outlook.com>
2020-02-01 20:58:39 -0800

    changed UI and reorganized project pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Platform, NavController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Storage } from '@ionic/storage';
 
-import { Pages } from './interfaces/pages';
 
 @Component({
   selector: 'app-root',
@@ -14,39 +13,15 @@ import { Pages } from './interfaces/pages';
 })
 export class AppComponent {
 
-  public appPages: Array<Pages>;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    public navCtrl: NavController,
     private storage: Storage,
     private router: Router
 
   ) {
-    this.appPages = [
-      {
-        title: 'Home',
-        url: '/home-results',
-        direct: 'root',
-        icon: 'home'
-      },
-      {
-        title: 'About',
-        url: '/about',
-        direct: 'forward',
-        icon: 'information-circle-outline'
-      },
-
-      {
-        title: 'App Settings',
-        url: '/settings',
-        direct: 'forward',
-        icon: 'cog'
-      }
-    ];
-
     this.initializeApp();
   }
 
@@ -64,11 +39,4 @@ export class AppComponent {
   }
 
   
-  goToEditProgile() {
-    this.navCtrl.navigateForward('edit-profile');
-  }
-
-  logout() {
-    this.navCtrl.navigateRoot('/');
-  }
 }
--- src/app/pages/home-results/home-results.page.ts
+++ src/app/pages/home-results/home-results.page.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-import {
-  NavController,
-  AlertController,
-  MenuController,
-  ToastController,
-  PopoverController,
-  ModalController } from '@ionic/angular';
+import {  ToastController, ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { PostProvider } from '../../../providers/post-provider';
 import { Storage } from '@ionic/storage';
 
 // Modals
-import { SearchFilterPage } from '../../pages/modal/search-filter/search-filter.page';
 import { ImagePage } from './../modal/image/image.page';
-// Call notifications test by Popover and Custom Component.
-import { NotificationsComponent } from './../../components/notifications/notifications.component';
+
 
 @Component({
   selector: 'app-home-results',
@@ -29,27 +20,24 @@ export class HomeResultsPage {
   username: string;
   anggota:any;
   constructor(
-    public navCtrl: NavController,
-    public menuCtrl: MenuController,
-    public popoverCtrl: PopoverController,
-    public alertCtrl: AlertController,
     public modalCtrl: ModalController,
     private router: Router,
-    private postPvdr: PostProvider,
     public toastCtrl : ToastController,
     private storage: Storage
   ) {
 
   }
 
+  // Just to display username on login
   ionViewWillEnter() {
-    this.menuCtrl.enable(true);
+    
     this.storage.get('session_storage').then((res)=>{
       this.anggota = res;
       this.username = this.anggota.username;
     })
   }
 
+  // Log out on click
   async prosesLogout(){
     this.storage.clear();
     this.router.navigate(['/login']);
@@ -60,56 +48,7 @@ export class HomeResultsPage {
     toast.present();
   }
 
-  settings() {
-    this.navCtrl.navigateForward('settings');
-  }
-
-  async alertLocation() {
-    const changeLocation = await this.alertCtrl.create({
-      header: 'Change Location',
-      message: 'Type your Address.',
-      inputs: [
-        {
-          name: 'location',
-          placeholder: 'Enter your new Location',
-          type: 'text'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Change',
-          handler: async (data) => {
-            console.log('Change clicked', data);
-            this.yourLocation = data.location;
-            const toast = await this.toastCtrl.create({
-              message: 'Location was change successfully',
-              duration: 3000,
-              position: 'top',
-              closeButtonText: 'OK',
-              showCloseButton: true
-            });
-
-            toast.present();
-          }
-        }
-      ]
-    });
-    changeLocation.present();
-  }
-
-  async searchFilter () {
-    const modal = await this.modalCtrl.create({
-      component: SearchFilterPage
-    });
-    return await modal.present();
-  }
-
+  // Shows popup of main image
   async presentImage(image: any) {
     const modal = await this.modalCtrl.create({
       component: ImagePage,
